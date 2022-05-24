a392a156ced64bfb783e1d9d9df5f86104cf4264
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-24 16:49:12 +0200

    create pivot table

---
+++ database/factories/CategoryPostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\CategoryPost;
+use Faker\Generator as Faker;
+
+$factory->define(CategoryPost::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
